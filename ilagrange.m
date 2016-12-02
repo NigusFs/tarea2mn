17f28aa2ac319f5bdb9fea4f77c23370132205ef
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-02 20:23:12 -0300

    Lagrange
    Saque la idea textual de un video de youtube

--- ilagrange.m
+++ ilagrange.m
@@ -1,17 +1,16 @@
-function [s1]=ilagrange(X,Y)% X valores de x, Y valores de y :v, 
-                            %nose si los Y los dan o hay q sacarlos,
-                            %revisar!
-   
-  [n,m]=size(X);
-   for i=1:1:n
-      p1=1;
-      s1=0;
-        while i<=n
-            if k ==i: % esta cosa va a tirar un error, corregir
-                i=i+1;
+function [C]=ilagrange(X,Y)
+%idea sacada de internet  codigos metodos numericos
+    [n,m]=size(X);
+    L=zeros(m);
+    for k=1:1:m
+        p1=1;
+        for j=1:m
+            if k ~=j
+              p1=conv(p1,poly(X(j)))/(X(k)-X(j));
             end
-            p1=p1*((x-X(i))/(X(k)-X(i)));
         end
-        s1=s1+p1*Y(k);
+        L(k,:)=p1;
     end
+
+C=Y*L;
 end
