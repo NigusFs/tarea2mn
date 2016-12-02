3e585dabd557ee2b54752f9b79e547d83991b073
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-01 20:31:11 -0400

    Metodo de Relajacion (SOR)
    Es lo mismo q gauss seidel pero este tiene un w asociado, que permite q el metodo converga mas rapido

---
+++ relajacion.m
@@ -0,0 +1,18 @@
+function [X1]=relajacion(A,B,X,w,e) %A matriz ,B vector columna,e error
+    [n,m]=size(A);
+    X1=B;% se iguala a B para dps setear los valores en casa posicion
+    s1=0;
+    s2=0;
+    while norm(X1-X,1)/norm(X1,1)<=e % error dado en la tarea
+        for k=1:1:n
+            for j=(k+1):1:n%primera sumatoria
+                s1=s1+(A(k,j)*X(j));
+            end
+            for j=1:1:k-1%segunda sumatoria
+                s2=s2+(A(k,j)*X1(j));
+            end
+            X1(k)=((w/A(k,k))*(B(k)- s1 - s2)/A(k,k))+((1-w)*X(k));
+        end
+        X=X1;
+    end
+end
