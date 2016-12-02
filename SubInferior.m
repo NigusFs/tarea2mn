0beda4af173bb815325832313f93a21cb8774815
Droamstart <againbllt_leandshoot@live.cl>
2016-12-02 01:49:04 -0300

    Update SubInferior.m

--- SubInferior.m
+++ SubInferior.m
@@ -1,7 +1,12 @@
-function y = SubInferior(a,b)
+% La funcion resuelve el sistema lineal ax=b
+% "a" es triangular superior usando substitucion hacia atras
+% "a" es la matriz de coeficientes
+%"b" es un vector columna de constantes
+% "y" es un vector columna con la solucion
+function y = SubAtras(a,b) 
 n = length(b);
 y(n,1) = b(n)/a(n,n);
-for i i = n-1:-1:1
+for i = n-1:-1:1
     y(i,1)=(b(i)-a(i,i+1:n)*y(i+1:n,1))./a(i,i);
 end
 
