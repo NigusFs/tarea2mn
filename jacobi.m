be29c3b713c0ec2d10d414e3f6b867737be5d75a
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-01 21:23:22 -0300

    Update jacobi.m

--- jacobi.m
+++ jacobi.m
@@ -1,11 +1,11 @@
-function [X1]=jacobi(A,B,X,w) %A matriz ,B vector columna,w error
+function [X1]=jacobi(A,B,X,e) %A matriz ,B vector columna,e error
     [n,m]=size(A);
     I=eye(n);
     Q=eye(n);% Q invertida
     for i=1:1:n
         Q(i,i)=1/A(i,i);
     end
-    while norm(X1-X,1)/norm(X1,1)<=w % error dado en la tarea
+    while norm(X1-X,1)/norm(X1,1)<=e % error dado en la tarea
         X1=(I-A*Q)*X+Q*B;
         X=X1;
     end
