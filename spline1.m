0ded2c786bf67bdd5c5c038c6f1774f0c7da3f53
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-02 17:33:45 -0300

    Update spline1.m
    Coloque de donde saque la idea

--- spline1.m
+++ spline1.m
@@ -3,4 +3,6 @@ n=size(x);
 cs = spline(x,[0 y 0]);
 xx = linspace(x(1),x(n(2)),101);
 plot(x,y,'o',xx,ppval(cs,xx),'-');
+%idea sacada de:
+%https://www.mathworks.com/help/matlab/ref/spline.html
 end
