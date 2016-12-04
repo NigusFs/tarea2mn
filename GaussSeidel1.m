70a7fc139968a4e91e565fa9de61640e6d2c2e6f
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-04 18:57:59 -0300

    Update GaussSeidel1.m

--- GaussSeidel1.m
+++ GaussSeidel1.m
@@ -1,9 +1,25 @@
-function [X1]=GaussSeidel1(A,B,X,w) %A matriz ,B vector columna,w error
+function [X1]=GaussSeidel1(A,B,X) %A matriz ,B vector columna,w error
     [n,m]=size(A);
     I=eye(n);
-    Q=inv(tril(X));% Q invertida
-    while norm(X1-X,1)/norm(X1,1)<=w % error dado en la tarea
-        X1=(I-A*Q)*X+Q*B;
+    Q=inv(tril(A));% Q invertida
+    k=0;
+    X=X';
+    X1=X;
+    norma=1;
+    while norma>=0.000001 % error dado en la tarea
+        
+        k=k+1;
+        if k == 30000
+            break
+        end
+        f=(I-Q*A)*X;
+        size(X);
+        X1=f+Q*B;
+        norma=norm(X-X1);
         X=X1;
     end
+    X1
+    k
+    fprintf('%10.6f\n',norma);
+   
 end
