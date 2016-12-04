95ae55a008f4e422f9c01fc7db8e9b6f382fb4cd
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-04 17:12:41 -0400

    cholesky

---
+++ cholesky1.m
@@ -0,0 +1,44 @@
+function []=cholesky1(A,b)
+[n,m]=size(A);
+C=[A,b];
+    for k=1:n
+        suma1=0;
+        for p=1:k-1
+            suma1=suma1+L(k,p)*u(p,k);
+        end
+        L(k,k)=sqrt(A(k,k)-suma1);
+        u(k,k)=L(k,k); 
+        for i=k+1:n
+            suma2=0;
+            for q=1:k-1
+                suma2=suma2+L(i,q)*u(q,k);
+            end
+            L(i,k)=(A(i,k)-suma2)/L(k,k); %Se obtiene la Matriz L
+        end
+        for j=k+1:n
+            suma3=0;
+            for r=1:k-1
+                suma3=suma3+L(k,r)*u(r,j);
+            end
+            u(k,j)=(A(k,j)-suma3)/L(k,k); %Se obtiene la matriz U
+        end
+    end
+    producto=det(L)*det(u); %Determinante
+    if producto~=0
+        for i=1:n
+            suma=0;
+            for p=1:i-1
+                suma=suma+L(i,p)*z(p);
+            end
+            z(i)=(b(i)-suma)/L(i,i); %Se obtiene el vector Z
+        end
+        for i=n:-1:1
+            suma=0;
+            for p=i+1:n
+                suma = suma+u(i,p)*x(p);
+            end
+            x(i)=(z(i)-suma)/u(i,i); % solucion del calculo de las variables
+        end   
+        x
+    end
+end
