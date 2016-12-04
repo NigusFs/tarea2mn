653d0754bf8ebf7e314703b8f4f685305c1c23c9
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-04 19:07:14 -0300

    Update richardson.m

--- richardson.m
+++ richardson.m
@@ -1,8 +1,20 @@
-function [X1]=richardson(A,B,X,e) %A matriz ,B vector columna,e error
+function []=richardson(A,B,X) %A matriz ,B vector columna,
 [n,m]=size(A);
 I=eye(n);
-    while norm(X1-X,1)/norm(X1,1)<=e
-        X1=(I-A)*X+B;
-        X=X1;
-    end
+X=X';
+X1=X;
+norma=1;
+i=0;
+while norma >= 0.00000001
+        i=i+1;
+       if i == 3000000
+            break
+       end
+       X1=(I-A)*X+B;
+       norma=norm(X-X1);
+       X=X1;    
+end
+   X1
+    i
+    fprintf('%10.6f\n',norma);
 end
