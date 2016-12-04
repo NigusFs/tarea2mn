f12fcf894865a5fd996dbfd96305106cb15ec72e
Nicolas Flores <nicolas.floress@mail.udp.cl>
2016-12-04 18:09:14 -0400

    Add files via upload

---
+++ grafico.m
@@ -0,0 +1,18 @@
+function []=grafico()
+    pp=0;
+    arrn=10:60;
+    arrnc=10:60;
+    for i=10:1:60
+        pp=pp+1;
+        A=full(gallery('tridiag',i,1,-4,1));
+        for j=4:i
+            A(j-3,j)=1;
+        end
+        for j=4:i
+            A(j,j-3)=1;
+        end
+        k=norm(A,Inf)*norm(inv(A),Inf);
+        arrnc(pp)=k;
+    end
+    plot(arrn,arrnc)
+end
